659902d0a18160030b7f594e3bd4dc40afd392ec
Siebrand-HHD <51705528+Siebrand-HHD@users.noreply.github.com>
2025-07-02 09:45:14 +0000

    add wandeling.geojson

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 console.log("Ook hallo wereld vanaf Javascript!");
 
+// docs:
+// https://github.com/search?q=maplibre&type=repositories
+// https://docs.geostandaarden.nl/serv/vt/
 // import * as maplibregl from "https://esm.sh/maplibre-gl";
 import * as maplibregl from "https://cdn.skypack.dev/maplibre-gl";
 
 const map = new maplibregl.Map({
     container: 'mijnkaart', // container id
     // style: 'https://demotiles.maplibre.org/style.json', // style URL
-    style: 'https://tiles.openfreemap.org/styles/liberty',
+    // style: 'https://tiles.openfreemap.org/styles/liberty',
+    style: 'assets/style.json',
     center: [0, 0], // starting position [lng, lat]
     zoom: 1 // starting zoom
 });
