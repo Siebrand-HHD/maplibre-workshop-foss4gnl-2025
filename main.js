416263d5339669a406ab0ab1c00aaf9eaec89d80
Siebrand-HHD <51705528+Siebrand-HHD@users.noreply.github.com>
2025-07-02 09:47:10 +0000

    update main.js

--- main.js
+++ main.js
@@ -5,12 +5,15 @@ console.log("Ook hallo wereld vanaf Javascript!");
 // https://docs.geostandaarden.nl/serv/vt/
 // import * as maplibregl from "https://esm.sh/maplibre-gl";
 import * as maplibregl from "https://cdn.skypack.dev/maplibre-gl";
+import { Protocol } from "https://esm.sh/pmtiles";
+const protocol = new Protocol();
+maplibregl.addProtocol("pmtiles", protocol.tile);
 
 const map = new maplibregl.Map({
     container: 'mijnkaart', // container id
     // style: 'https://demotiles.maplibre.org/style.json', // style URL
     // style: 'https://tiles.openfreemap.org/styles/liberty',
     style: 'assets/style.json',
-    center: [0, 0], // starting position [lng, lat]
-    zoom: 1 // starting zoom
+    center: [5.66342, 51.97374], // starting position [lng, lat]
+    zoom: 10 // starting zoom
 });
